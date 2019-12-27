ef9255ec4465a0b9629b79590ae091014804b11e
vd28 <44005434+vd28@users.noreply.github.com>
2019-12-27 09:40:43 +0200

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('rating/persons/', views.PersonRatingView.as_view(), name='person_rating'),
+    path('rating/faculties/', views.FacultyRatingView.as_view(), name='faculty_rating'),
+    path('rating/departments/', views.DepartmentRatingView.as_view(), name='department_rating'),
+    path('persons/<int:person_id>/', views.PersonView.as_view(), name='person'),
+    path('persons/search/', views.PersonsSearchResultsView.as_view(), name='persons_search_results'),
+    path('config-not-found/', views.ConfigNotFoundView.as_view(), name='config_not_found'),
+    path('department-person/<int:department_id>', views.DepartmentPersonRatingView.as_view(), name='department_person_rating'),
+    path('faculty-department/<int:faculty_id>', views.FacultyDepartmentRatingView.as_view(), name='faculty_department_rating')
+]
---
+++ views.py
@@ -0,0 +1,88 @@
+from django.views.generic import TemplateView, View
+from django.views.generic.base import ContextMixin
+from django.urls import reverse
+from django.shortcuts import redirect
+
+from core import queries as core_queries
+from . import queries
+
+
+class ConfigMixin(ContextMixin, View):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.config = None
+
+    def dispatch(self, request, *args, **kwargs):
+        config = queries.fetch_active_config()
+        if config is None:
+            return redirect(reverse('config_not_found'))
+        self.config = config
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['config'] = self.config
+        data['university'] = self.config.university
+        data['revision'] = self.config.revision
+        return data
+
+
+class ConfigNotFoundView(TemplateView):
+    template_name = 'user_site/config_not_found.html'
+
+
+class HomeView(ConfigMixin, TemplateView):
+    template_name = 'user_site/home.html'
+
+
+class PersonRatingView(ConfigMixin, TemplateView):
+    template_name = 'user_site/person_rating.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['person_types'] = tuple(core_queries.fetch_person_types())
+        return data
+
+
+class FacultyRatingView(ConfigMixin, TemplateView):
+    template_name = 'user_site/faculty_rating.html'
+
+
+class DepartmentRatingView(ConfigMixin, TemplateView):
+    template_name = 'user_site/department_rating.html'
+
+class DepartmentPersonRatingView(ConfigMixin,TemplateView):
+    template_name = 'user_site/department_person_rating.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['person_types'] = tuple(core_queries.fetch_person_types())
+        data['department'] = core_queries.fetch_department(self.kwargs['department_id'])
+        return data
+
+class FacultyDepartmentRatingView(ConfigMixin, TemplateView):
+    template_name = 'user_site/faculty_department_rating.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['faculty']=core_queries.fetch_faculty(self.kwargs['faculty_id'])
+        return data
+
+
+class PersonView(ConfigMixin, TemplateView):
+    template_name = 'user_site/person.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['person'] = core_queries.fetch_person(self.kwargs['person_id'], load_university=True)
+        return data
+
+
+class PersonsSearchResultsView(ConfigMixin, TemplateView):
+    template_name = 'user_site/persons_search_results.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['persons'] = tuple(core_queries.search_persons(self.request.GET.get('t', None)))
+        return data
